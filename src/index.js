ce3643967b26304aea62ca3c4e3de5ba4196986b
myk245 <myk245@nyu.edu>
2020-02-19 15:56:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const inputField = event.target.querySelector("#new-task-description")
+    const inputString = inputField.value
+    const taskTag = document.createElement("li")
+    taskTag.textContent = inputString 
+
+    const tasks = document.getElementById("tasks")
+    tasks.appendChild(taskTag)
+  })
 });
+
+
+// I should be able to type a task into the input field.
+// I should be able to click some form of a submit button.
+// the task string that I provided should appear on the DOM after the submit button has been activated.
